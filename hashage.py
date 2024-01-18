759d2ec36dc4fb515db948a3821eda10473e6655
Jeancy shabani <98698918+Jeancy1909@users.noreply.github.com>
2024-01-18 23:18:08 +0100

    Ajout ses noms des membres du groupe

--- hashage.py
+++ hashage.py
@@ -1,3 +1,14 @@
+#1.Kukungama Kanyenge Nathan 
+#2. Malu Mwanza Ariel 
+#3. Makwaya Mawonda Mauricette 
+#4. Senkere Afidou Jemima 
+#5. Luayi Kangombo Jonas 
+#6. Mubiala Kiesse Samuel 
+#7. Ngamba Nkua-Mvibudulu Dave 
+#8. Kabisayi Katambua Franklin 
+#9. Nshole Mazana Obed 
+#10. Shabani Bin Shabani Jeancy
+
 
 ### SECTION DE CONVERSION EN VALEUR ASCII (SUR 3 CHIFFRES)
 def to_ascii_bin(p):
